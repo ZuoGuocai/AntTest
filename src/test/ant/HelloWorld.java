0f5a1d59655f61a0d44d97e6815ff40f29bee400
Zuo Guocai <zuoguocai@126.com>
2017-05-22 08:19:24 +0800

    first commit

---
+++ src/test/ant/HelloWorld.java
@@ -0,0 +1,7 @@
+package test.ant;  
+public class HelloWorld {  
+    public static void main(String[] args) {  
+        // TODO Auto-generated method stub  
+        System.out.println("HelloWord !!");  
+    }  
+} 
